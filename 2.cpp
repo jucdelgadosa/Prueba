c4549c0698fa3044464e357da4efc2ae681ef884
jucdelgadosa <jucdelgadosa@unal.edu.co>
2017-08-27 11:54:10 -0500

    segunda versión del programa

--- 2.cpp
+++ 2.cpp
@@ -4,39 +4,38 @@
 #include<cmath>
 #include<cstdlib>
 
-double p()
+double p(double x)
 {
-  std::cout.precision(16);
-  std::cout.setf(std::ios::scientific);
-  double p, x=0.01;
+  double p;
   p = std::pow(1-x,8);
-  std::cout<<p<<"\n";
+  return p;
 }
 
-double q()
+double q(double x)
 {
-  std::cout.precision(16);
-  std::cout.setf(std::ios::scientific);
-  double q, x=0.01;
-  q = 1 - 8*x + 28*x*x - 56*x*x*x + 70*x*x*x*x - 56*x*x*x*x*x + 28*x*x*x*x*x*x - 8*x*x*x*x*x*x*x + x*x*x*x*x*x*x*x;
-  std::cout<<q<<"\n";
+  double q;
+  q = 1 - (8*x) + (28*x*x) - (56*x*x*x) + (70*x*x*x*x) - (56*x*x*x*x*x) + (28*x*x*x*x*x*x) - (8*x*x*x*x*x*x*x) + (x*x*x*x*x*x*x*x);
+  return q;
 }
 
-double r()
+double r(double x)
 {
-  std::cout.precision(16);
-  std::cout.setf(std::ios::scientific);
-  double r, x=0.01;
-  r = 1 - 8*x + 28*std::pow(x,2) - 56*std::pow(x,3) + 70*std::pow(x,4) - 56*std::pow(x,5) + 28*std::pow(x,6) - 8*std::pow(x,7) + std::pow(x,8);
-  std::cout<<r<<"\n";
+  double r;
+  r = 1 - (8*x) + (28*std::pow(x,2)) - (56*std::pow(x,3)) + (70*std::pow(x,4)) - (56*std::pow(x,5)) + (28*std::pow(x,6)) - (8*std::pow(x,7)) + (std::pow(x,8));
+  return r;
 }
 
 int main()
 {
-  p();
-  q();
-  r();
+  std::cout.precision(16);
+  std::cout.setf(std::ios::scientific);
+  
+  for (double n=0.992; n<= 1.008; n+=0.0001)
+    {
+      std::cout<<n<<"\t"<<p(n)<<"\t"<<q(n)<<"\t"<<r(n)<<"\n";
+    }
   return 0;
 }
 
   
+
